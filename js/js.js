7b6bba0e655dc89edd3db1cb0244d6ea9399dc1f
AlessandroDias1979 <alessandroddigital@gmail.com>
2025-09-16 21:33:17 -0300

    feat: mudança 0001-2

--- js/js.js
+++ js/js.js
@@ -6,8 +6,6 @@ let subtracao = 0;
 let saldo = 0; 
 
 
-                                                         
-
 for (let i = 0; i < deposito.length; i++) {
     soma += deposito[i];
 }
@@ -22,9 +20,9 @@ for (let i = 0; i < saque.length; i++) {
 
 console.log(`O usuário é: ${usuario}`);
 console.log(`O total de saques é: R$${subtracao}`);
-
-
 console.log(`O saldo final é: R$${saldo}`);
+
+/* constantes para conversão*/
 
 const moeda = saldo;
 let real = moeda;
@@ -32,8 +30,46 @@ let dolar = moeda / 6;
 let euro = moeda / 7;     
 
 /* conversão de moedas */
+/* toFixed(2) -> limita o número de casas decimais para 2 */
 
 console.log(`O saldo em real é: R$${real}`);
-console.log(`O saldo em dólar é: U$${dolar}`);
-console.log(`O saldo em euro é: €${euro}`);
+console.log(`O saldo em dólar é: U$${dolar.toFixed(2)}`);
+console.log(`O saldo em euro é: €${euro.toFixed(2)}`);
 
+
+/* Função para calcular o valor da compra com base em convênios e cartão da loja */
+
+function calcularValorDaCompra(valorDaCompra, possuiOuNaoConvenio, compraOuNaoComCartaoDaLoja){
+    if(possuiOuNaoConvenio && compraOuNaoComCartaoDaLoja){
+        return valorDaCompra - valorDaCompra * 0.15;
+}
+    else if(possuiOuNaoConvenio || compraOuNaoComCartaoDaLoja){
+        return valorDaCompra - valorDaCompra * 0.10;
+    } else {
+
+        return valorDaCompra;
+
+    }
+}
+
+/*resultados dos testes*/
+
+console.log(calcularValorDaCompra(100, true, true)); // 15
+console.log(calcularValorDaCompra(100, true, false)); // 10
+console.log(calcularValorDaCompra(100,false,false)); // 0
+
+/* Função para encontrar o ano mais próximo */
+
+const anos = ['2012', '2010', '2020', '2022', '2035', '2045'];
+function encontrarAno(ano){ 
+    
+    const resultado = [];
+    
+    for(let i = 0; i < anos.length; i++){
+        if(anos[i] <= ano){
+
+            return anos[i];
+        }
+    } 
+}
+console.log(encontrarAno(2021));
